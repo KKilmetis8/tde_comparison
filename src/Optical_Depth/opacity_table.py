6ea043fd31f10cf6e1ae161f321a7dc579c5927e
Konstantinos Kilmetis <43760101+KKilmetis8@users.noreply.github.com>
2023-09-12 18:59:18 +0200

    Refactored opacities
    Opacities work better now

--- src/Optical_Depth/opacity_table.py
+++ src/Optical_Depth/opacity_table.py
@@ -10,22 +10,73 @@ import numpy as np
 from scipy.interpolate import RegularGridInterpolator
 
 # All units are ln[cgs]
-loadpath = 'src/Ionization/'
+loadpath = 'src/Optical_Depth/'
 lnT = np.loadtxt(loadpath + 'T.txt')
 lnrho = np.loadtxt(loadpath + 'rho.txt')
-lnk = np.loadtxt(loadpath + 'ross.txt') # rosseland mean
+lnk_ross = np.loadtxt(loadpath + 'ross.txt')
+lnk_plank = np.loadtxt(loadpath + 'plank.txt')
+lnk_scatter = np.loadtxt(loadpath + 'scattering.txt')
 
-lnk_inter = RegularGridInterpolator( (lnT, lnrho), lnk)
+lnk_ross_inter = RegularGridInterpolator( (lnT, lnrho), lnk_ross)
+lnk_plank_inter = RegularGridInterpolator( (lnT, lnrho), lnk_plank)
 
-def opacity(rho, T):
-    
-    # Turn to ln 
-    # ln_temp = np.log(T)
-    # ln_rho = np.log(rho)
-    
-    # Use interpolated function
-    ln_opacity = lnk_inter((T, rho))
-    
+def opacity(rho, T, kind, ln = False):
+    '''
+    Parameters
+    ----------
+    rho : float,
+        Density in [cgs].
+    T : float,
+        Temperature in [cgs].
+    kind : str,
+        The kind of opacities. Valid choices are:
+        rosseland, plank or effective.
+    log : bool,
+        If True, then T and rho are lnT and lnrho
+
+    Returns
+    -------
+    opacity : float,
+        The rosseland mean opacity in [cgs].
+    '''    
+    if ln:
+        
+        # Pick Opacity & Use Interpolation Function
+        if kind == 'rosseland':
+            ln_opacity = lnk_ross_inter((T, rho))
+            
+        elif kind == 'plank':
+            ln_opacity = lnk_plank_inter((T, rho))
+            
+        elif kind == 'effective':
+            plank = lnk_plank_inter((T, rho))
+            scattering = lnk_scatter((T, rho))
+            
+            # Rybicky & Lightman eq. 1.98
+            ln_opacity = np.sqrt(plank * (plank + scattering)) 
+        else:
+            print('Invalid opacity type. Try rosseland, plank or effective.')
+            return 1
+        
+    else:
+        # Turn to ln 
+        T = np.log(T)
+        rho = np.log(rho)
+        
+        # Pick Opacity & Use Interpolation Function
+        if kind == 'rosseland':
+            ln_opacity = lnk_ross_inter((T, rho))
+            
+        elif kind == 'plank':
+            ln_opacity = lnk_plank_inter((T, rho))
+            
+        elif kind == 'effective':
+            plank = lnk_plank_inter((T, rho))
+            ross = lnk_ross_inter((T, rho))
+            
+            # Rybicky & Lightman eq. 1.98
+            ln_opacity = np.sqrt(plank * (plank + scattering)) 
+            
     # Remove the ln
     opacity = np.exp(ln_opacity)
     
