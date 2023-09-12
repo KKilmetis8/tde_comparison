a8aa1c7c1c0fc416493d47b5c7d81718e6930a1b
spaceturtle707 <p.martire98@gmail.com>
2023-09-12 16:14:58 +0200

    modifies in emissivity.py, written blue.py for the last step

--- src/Luminosity/emissivity.py
+++ src/Luminosity/emissivity.py
@@ -1,5 +1,5 @@
 """
-Calculate emissivity for a cell
+Calculate emissivity for A SINGLE cell.
 """
 
 import numpy as np
@@ -12,7 +12,10 @@ G = 6.6743e-11 #SI
 c = 2.9979e10 #[cm/s] 
 t_conv = np.sqrt((solar_radius/10**2)**3/(G*solar_mass*10**(-3))) #time in seconds is time_simlation*t_conv
 
-"""Import data from simulation. NB: Planck, T, rho are in ln"""
+"""
+Import data from simulation and interpolate.
+NB: Planck, T, rho are in ln
+"""
 loadpath = 'src/Optical_Depth/'
 lnT = np.loadtxt(loadpath + 'T.txt') 
 lnrho = np.loadtxt(loadpath + 'rho.txt')
@@ -21,7 +24,7 @@ lnplanck = np.loadtxt(loadpath + 'planck.txt')
 lnk_inter = RegularGridInterpolator( (lnT, lnrho), lnplanck) #we use T and rho to interpolate??
 
 def emissivity(T,rho, cell_vol):
-    """this function accepts arguments in CGS"""
+    """NB: this function accepts arguments in CGS"""
     ln_T = np.log(T)
     ln_rho = np.log(rho)
     ln_planck = lnk_inter((ln_T, ln_rho))
@@ -30,5 +33,5 @@ def emissivity(T,rho, cell_vol):
     return emiss
 
 if __name__ == "__main__":
-    test = emissivity(10**(7),10**(-10),2)
+    test = emissivity(10**7,10**(-10),2)
     print(test)
