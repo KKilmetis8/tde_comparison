af6b624a54002a2a1f793add8985a5fb4064699b
spaceturtle707 <p.martire98@gmail.com>
2023-09-20 00:05:58 +0200

    final changes to multiplot

--- Multiplot.py
+++ Multiplot.py
@@ -10,7 +10,7 @@ import numpy as np
 
 red_4 = np.loadtxt('reddata_m4.txt')
 blue_4 = np.loadtxt('bluedata_m4.txt')
-# red_6 = np.loadtxt('reddata_m6.txt')
+red_6 = np.loadtxt('reddata_m6.txt')
 blue_6 = np.loadtxt('bluedata_m6.txt')
 
 red_4_days = red_4[0]
@@ -20,17 +20,19 @@ blue_4_days = blue_4[0]
 blue_4_lum = np.log10(blue_4[1])
 
 
-# red_6_days = red_6[0]
-# red_6_lum = np.log10(red_6[1])
+red_6_days = red_6[0]
+red_6_lum = np.log10(red_6[1])
 
 blue_6_days = blue_6[0]
 blue_6_lum = np.log10(blue_6[1])
 
 plt.plot(red_4_days, red_4_lum, color = 'coral', label = "FLD $10^4M_{sun}$", linestyle = 'dashed')
 plt.plot(blue_4_days, blue_4_lum, color = 'deepskyblue', label = "BB $10^4M_{sun}$", linestyle = 'dashed')
-# plt.plot(red_6_days, red_6_lum, color = 'coral', label = "FLD $10^6M_{sun}$")
+plt.plot(red_6_days, red_6_lum, color = 'coral', label = "FLD $10^6M_{sun}$")
 plt.plot(blue_6_days, blue_6_lum, color = 'deepskyblue', label = "BB $10^6M_{sun}$")
-plt.ylabel("$log_{10}$L")
+plt.ylabel("$log_{10}$Luminosity")
 plt.xlabel("$t/t_{fb}$")
-plt.legend()
+plt.xlim(1,1.4)
+plt.legend(loc = 'lower right', fontsize = 8)
 plt.show()
+plt.savefig('multiplot.png')
