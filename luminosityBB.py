adf549c8671c34fccfd6bce78f1aef5d668e1be6
spaceturtle707 <p.martire98@gmail.com>
2023-09-22 00:56:46 +0200

    other tries

--- luminosityBB.py
+++ luminosityBB.py
@@ -69,7 +69,7 @@ def find_peak(Temperature):
     npeak = const_npeak * Temperature
     return 10*npeak
 
-def planck_fun_n_cell(Temperature: int, n: int) -> int:
+def planck_fun_n_cell(Temperature: float, n: float) -> float:
     """ Planck function in a cell. """
     const = 2*h/c**2
     peak = find_peak(Temperature)
@@ -79,19 +79,22 @@ def planck_fun_n_cell(Temperature: int, n: int) -> int:
         fun = const * n**3 / (np.exp(h*n/(Kb*Temperature))-1)
     return fun
 
-def planck_fun_cell(Temperature: int) -> int:
+def planck_fun_cell(Temperature: float) -> float:
     """
     Bolometric planck function in a cell. 
     We select the range for frequency in order to no overcome the peak or we have a mess.
     """
-    for n in n_array:
+    planck_fun_n_array = []
+    peak = find_peak(Temperature)
+    n_arr = np.linspace(n_min,peak,n_spacing)
+    for n in n_arr:
         planck_fun_n_array_single = planck_fun_n_cell(Temperature, n)
         planck_fun_n_array.append(planck_fun_n_array_single)
     planck_fun_n_array = np.array(planck_fun_n_array)
-    fun = np.trapz(planck_fun_n_array, n_array)
+    fun = np.trapz(planck_fun_n_array, n_arr)
     return fun
 
-def luminosity_n(Temperature: np.array, Density: np.array, tau: np.array, volume: np.array, n:int):
+def luminosity_n(Temperature: float, Density: float, tau: float, volume: float, n:int):
     """
     Temperature, Density and volume: np.array from near to the BH to far away. Thus we will use negative index in the for loop.
     tau: np.array from outside to inside.
@@ -99,44 +102,47 @@ def luminosity_n(Temperature: np.array, Density: np.array, tau: np.array, volume
 
     We obtain luminosity in a ray as function of frequency.
     """
-    lum = 0
-    for i in range(len(tau)):              
-        T = Temperature[-i]
-        rho = Density[-i] 
-        opt_depth = tau[i]
-        cell_vol = volume[-i]
+    # lum = 0
+    # for i in range(len(tau)):              
+    #     T = Temperature[-i]
+    #     rho = Density[-i] 
+    #     opt_depth = tau[i]
+    #     cell_vol = volume[-i]
 
-        # Ensure we can interpolate
-        rho_low = np.exp(-22)
-        T_low = np.exp(8.77)
-        T_high = np.exp(17.8)
-        if rho < rho_low or T < T_low or T > T_high:
-            continue
+    #     # Ensure we can interpolate
+    #     rho_low = np.exp(-22)
+    #     T_low = np.exp(8.77)
+    #     T_high = np.exp(17.8)
+    #     if rho < rho_low or T < T_low or T > T_high:
+    #         continue
         
-        epsilon = emissivity(T, rho, cell_vol)
-        lum_cell = epsilon * planck_fun_n_cell(T, n) * np.exp(-opt_depth)
-        lum += lum_cell
-    return (lum/planck_fun_cell(T))
+    epsilon = emissivity(Temperature, Density, volume)
+    lum_cell = epsilon * planck_fun_n_cell(Temperature, n) * np.exp(-tau)
+    return (lum_cell/planck_fun_cell(Temperature))
 
-def luminosity(Temperature: np.array, Density: np.array, tau: np.array, volume: np.array) -> int:
+def luminosity(Temperature: float, Density: float, tau: float, volume: float) -> int:
     """Gives NOT normalised bolometric luminosity in a ray."""
-    for n in n_array:
+    lum_n_array = []
+    peak = find_peak(Temperature)
+    n_arr = np.linspace(n_min,peak,n_spacing)
+    for n in n_arr:
         value = luminosity_n(Temperature, Density, tau, volume, n)
         lum_n_array.append(value)
     lum_n_array = np.array(lum_n_array)
-    lum = np.trapz(lum_n_array, n_array)
+    lum = np.trapz(lum_n_array, n_arr)
     return lum 
     
-def normalised_luminosity_n(Temperature: np.array, Density: np.array, tau: np.array, volume: np.array, n: int, luminosity_fld: int):
-    """
-    luminosity_fld: float. It's the luminosity with FLD method from the considered snapshot.
-    Gives the luminosity normalised with FLD model. """
-    norm = luminosity_fld / luminosity(Temperature, Density,  tau, volume)
-    value = luminosity_n(Temperature, Density,  tau, volume, n)
-    # value = np.nan_to_num(value) #maybe we don't need this beacuse we've selected the range of freuency
-    return value * norm
+# def normalised_luminosity_n(Temperature: float, Density: float, tau: float, volume: float, n: float, luminosity_fld: float):
+#     """
+#     luminosity_fld: float. It's the luminosity with FLD method from the considered snapshot.
+#     Gives the luminosity normalised with FLD model. """
+#     norm = luminosity_fld / luminosity(Temperature, Density,  tau, volume)
+#     value = luminosity_n(Temperature, Density,  tau, volume, n)
+#     # value = np.nan_to_num(value) #maybe we don't need this beacuse we've selected the range of freuency
+#     return value * norm
 
 if __name__ == "__main__":
+    
     #CHECK PLANCK
     # check_planck_n = []
     # for n in n_array:
@@ -150,28 +156,57 @@ if __name__ == "__main__":
     luminosity_fld_fix = fld_data[1]
     rays_den, rays_T, rays_tau, photosphere, radii = get_photosphere(fix, m)
     dr = (radii[1] - radii[0]) * Rsol_to_cm
-    volume = 4 * np.pi * radii**2 * dr  / 192 
-    
-    lum_tilde_n = []
-    for n in n_array:
-        lum_n = 0
-        for i, ray in enumerate(rays_den):
-            lum = normalised_luminosity_n(rays_T[i],  rays_den[i], rays_tau[i], 
-                                    volume, n, luminosity_fld_fix[0])
-            lum_n += lum
-    lum_tilde_n.append(lum_n)
+    volume = 4 * np.pi * radii**2 * dr  / 192
+    print(np.array(rays_T).shape) 
+    print(np.array(volume).shape)
+    # for n in n_array:
+    #     a = normalised_luminosity_n(1e4, 1e-10,1,1e-5,n,10)
+    #     print(a)
+
+    lum_tilde_n = np.zeros(len(n_array))
+    for j in range(len(rays_den)):
+        for i in range(len(rays_tau[j])):              
+            T = rays_T[j][-i]
+            rho = rays_den[j][-i] 
+            opt_depth = rays_tau[j][i]
+            cell_vol = volume[-i]
+
+            # Ensure we can interpolate
+            rho_low = np.exp(-22)
+            T_low = np.exp(8.77)
+            T_high = np.exp(17.8)
+            if rho < rho_low or T < T_low or T > T_high:
+                continue
+
+            norm = luminosity_fld_fix[0] * luminosity(T, rho, opt_depth, cell_vol)
+            for i in range(len(n_array)):
+                lum_nu_cell = luminosity_n(T, rho, opt_depth, cell_vol, n_array[i])
+                lum_tilde_n[i] += lum_nu_cell
+        print(j)
 
     # lum_tilde_n = []
     # for n in n_array:
-    #     lum_ray = 0 
-    #     for i, ray in enumerate(rays_den):
-    #         lum_one_ray = 0
-    #         for j in range(0,len(rays_T[i])):
-    #             lum = normalised_luminosity_n(rays_T[i][j],  rays_den[i][j], rays_tau[i][j], 
-    #                                     volume, n, luminosity_fld_fix[0])
-    #             lum_one_ray += lum
-    #         lum_ray += lum_one_ray
-    #     lum_tilde_n.append(lum_ray)
-    plt.plot(n_array, np.array(lum_tilde_n))
+    #     lum_rays = 0 
+    #     for j in range(len(rays_den)):
+    #         for i in range(len(rays_tau[j])):              
+    #             T = rays_T[j][-i]
+    #             rho = rays_den[j][-i] 
+    #             opt_depth = rays_tau[j][i]
+    #             cell_vol = volume[-i]
+
+    #             # Ensure we can interpolate
+    #             rho_low = np.exp(-22)
+    #             T_low = np.exp(8.77)
+    #             T_high = np.exp(17.8)
+    #             if rho < rho_low or T < T_low or T > T_high:
+    #                 continue
+
+    #             #luminosity in a cell
+    #             lum = normalised_luminosity_n(T,  rho, opt_depth, 
+    #                                     cell_vol, n, luminosity_fld_fix[0])
+    #             lum_rays += lum
+    #         print(j)
+    #     lum_tilde_n.append(lum_rays)
+    plt.plot(np.log10(n_array), np.log10(lum_tilde_n))
     plt.show()
 
