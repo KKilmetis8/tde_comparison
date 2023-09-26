11cd52a9c2abb40b77500e84c293fefd274446fe
spaceturtle707 <p.martire98@gmail.com>
2023-09-26 12:30:13 +0200

    plot

--- L_tilde_n6.py
+++ L_tilde_n6.py
@@ -10,10 +10,10 @@ L_tilda_n881 = L_tilda_n[2]
 L_tilda_n925 = L_tilda_n[3]
 L_tilda_n950 = L_tilda_n[4]
 
-plt.plot(n_logspace, L_tilda_n844, c = 'tomato', label = 't/tf = 1')
-plt.plot(n_logspace, L_tilda_n881, c = 'orange', label = 't/tf = 1.14')
-plt.plot(n_logspace, L_tilda_n925, c = 'yellowgreen', label = 't/tf = 1.3')
-plt.plot(n_logspace, L_tilda_n950, c = 'teal', label = 't/tf = 1.4')
+plt.plot(n_logspace, L_tilda_n844, c = 'tomato', label = 't/$t_{fb}$ = 1')
+plt.plot(n_logspace, L_tilda_n881, c = 'orange', label = 't/$t_{fb}$ = 1.14')
+plt.plot(n_logspace, L_tilda_n925, c = 'yellowgreen', label = 't/$t_{fb}$ = 1.3')
+plt.plot(n_logspace, L_tilda_n950, c = 'teal', label = 't/$t_{fb}$ = 1.4')
 
 plt.xlabel(r'$log\nu$ [Hz]')
 plt.xlim(12,18)
@@ -21,9 +21,9 @@ plt.ylim(1e38, 1e45)
 plt.yscale('log')
 plt.ylabel(r'$log\tilde{L}_\nu$ [erg/s]')
 plt.grid()
-plt.savefig('TOT_Ltilda_m' + str(m) )
-plt.show()
+plt.savefig('TOT_Ltilda_m' + str(m) + '.png' )
 plt.legend()
+plt.show()
 plt.axvline(15, color = 'tab:orange')
 plt.axvline(17, color = 'tab:orange')
 plt.axvspan(15, 17, alpha=0.5, color = 'tab:orange')
