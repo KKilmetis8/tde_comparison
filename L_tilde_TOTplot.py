98dedb810736f60733452938db48b896c03c48b9
spaceturtle707 <p.martire98@gmail.com>
2023-09-28 15:53:31 +0200

    lot of plots

--- L_tilde_TOTplot.py
+++ L_tilde_TOTplot.py
@@ -1,115 +1,67 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-m = 6
+m = 4
 plot = 'bolometric'
+n_min = 1e12 
+n_max = 1e18
+n_spacing = 10000
+n_array = np.linspace(n_min, n_max, num = n_spacing)
+
 L_tilda_n = np.loadtxt('Ltilda_m'+ str(m) + '.txt')
-n_array = np.linspace(1e12, 1e19, num = 100)
-n3 = n_array**3
-print(n3)
-n4 = n_array**4
+bolom = bolom = np.loadtxt('L_m'+ str(m) + '.txt')
 
-n_logspace = L_tilda_n[0]
-
-if m == 6:
-    L_tilda_n844 = L_tilda_n[1]
-    L_tilda_n881 = L_tilda_n[2]
-    L_tilda_n925 = L_tilda_n[3]
-    L_tilda_n950 = L_tilda_n[4]
-
-    L844 = np.trapz(L_tilda_n844, n_array)
-    L881 = np.trapz(L_tilda_n881, n_array)
-    L925 = np.trapz(L_tilda_n925, n_array)
-    L950 = np.trapz(L_tilda_n950, n_array)
-
-    if plot == 'bolometric':
+if plot == 'bolometric':
+    if m == 6:
         #fixes6 = [844, 881, 925, 950]
-        days6 = [1, 1.14, 1.3, 1.4] #t/t_fb
-        bolom = []
-        
-        bolom.append(L844)
-        bolom.append(L881)
-        bolom.append(L925)
-        bolom.append(L950)
+        days = [1, 1.14, 1.3, 1.4] #t/t_fb
+    if m == 4:
+        #fixes4 = [177, 233,]
+        days = [1, 1.2, 1.3] #t/t_fb
 
-        with open('L6.txt', 'a') as f:
-            f.write(' '.join(map(str, days6))+'\n')
-            f.write(' '.join(map(str, bolom)) + '\n')
-            f.close()
-
-        plt.plot(days6,bolom, '-o')
-        plt.ylabel(r'$log_{10}$ Luminosity [erg/s]')
-        plt.xlabel(r'$t/t_{fb}$')
-        #plt.ylim(1e42,2e45)
-        plt.yscale('log')
-
-        
-    if plot == 'spectra':
-        plt.plot(n_logspace, L_tilda_n844, c = 'tomato', label = 't/$t_{fb}$ = 1')
-        plt.plot(n_logspace, L_tilda_n881, c = 'orange', label = 't/$t_{fb}$ = 1.14')
-        plt.plot(n_logspace, L_tilda_n925, c = 'yellowgreen', label = 't/$t_{fb}$ = 1.3')
-        plt.plot(n_logspace, L_tilda_n950, c = 'teal', label = 't/$t_{fb}$ = 1.4')
-        plt.plot(n_logspace, n3)
-        plt.plot(n_logspace, n4)
+    plt.plot(days, bolom, '-o')
+    plt.ylabel(r'$log_{10}$ Luminosity [erg/s]')
+    plt.xlabel(r'$t/t_{fb}$')
+    #plt.ylim(1e42,2e45)
+    plt.yscale('log')
+    plt.grid()
+    plt.savefig('BolomLtilda_m' + str(m) + '.png' )
 
 
-        plt.xlim(12,19)
-        plt.ylim(1e35, 1e45)
-        plt.xlabel(r'$log\nu$ [Hz]')
-        plt.yscale('log')
-        plt.ylabel(r'$log\tilde{L}_\nu$ [erg/s]')
-
-if m == 4:
-    #L_tilda_n177 = L_tilda_n[4]
-    L_tilda_n233 = L_tilda_n[1]
-    L_tilda_n254 = L_tilda_n[2]
-    L_tilda_n263 = L_tilda_n[3]
-
-    L233 = np.trapz(L_tilda_n233, n_logspace)
-    L254 = np.trapz(L_tilda_n254, n_logspace)
-    L263 = np.trapz(L_tilda_n263, n_logspace)
-    #L177 = np.trapz(L_tilda_n177, n_logspace)
-
-    if plot == 'bolometric':
-        #fixes6 = [233, 254, 263]
-        days4 = [1, 1.2, 1.3] #t/t_fb
-        bolom = []
-        
-        bolom.append(L233)
-        bolom.append(L254)
-        bolom.append(L263)
-        # bolom.append(L950)
-
-        with open('L4.txt', 'a') as f:
-            f.write(' '.join(map(str, days4))+'\n')
-            f.write(' '.join(map(str, bolom)) + '\n')
-            f.close()
-            
-        plt.plot(days4,bolom, '-o')
-        plt.ylabel(r'$log_{10}$ Luminosity [erg/s]')
-        plt.xlabel(r'$t/t_{fb}$')
-        plt.ylim(1e41,1e42)
-        plt.yscale('log')
-
-        
-    if plot == 'spectra':
-
-        plt.plot(n_logspace, L_tilda_n233, c = 'tomato', label = 't/$t_{fb}$ = 1')
-        plt.plot(n_logspace, L_tilda_n254, c = 'orange', label = 't/$t_{fb}$ = 1.2')
-        plt.plot(n_logspace, L_tilda_n263, c = 'yellowgreen', label = 't/$t_{fb}$ = 1.3')
-        #plt.plot(n_logspace, L_tilda_n263, c = 'teal', label = 't/$t_{fb}$ = 1.4')
-        plt.xlabel(r'$log\nu$ [Hz]')
-        plt.yscale('log')
-        plt.ylabel(r'$log\tilde{L}_\nu$ [erg/s]')
-        plt.xlim(12,18)
-        plt.ylim(1e32, 1e42)
+if plot == 'spectra':
+    if m == 6:
+        L_tilda_n844 = L_tilda_n[1]
+        L_tilda_n881 = L_tilda_n[2]
+        L_tilda_n925 = L_tilda_n[3]
+        L_tilda_n950 = L_tilda_n[4]
+        plt.plot(n_array, L_tilda_n844, c = 'tomato', label = 't/$t_{fb}$ = 1')
+        plt.plot(n_array, L_tilda_n881, c = 'orange', label = 't/$t_{fb}$ = 1.14')
+        plt.plot(n_array, L_tilda_n925, c = 'yellowgreen', label = 't/$t_{fb}$ = 1.3')
+        plt.plot(n_array, L_tilda_n950, c = 'teal', label = 't/$t_{fb}$ = 1.4')
+        # plt.plot(n_array, n3)
+        # plt.plot(n_array, n4)
+        #plt.ylim(1e35, 1e45)
 
 
-plt.grid()
-plt.legend()
+    if m == 4:
+        L_tilda_n233 = L_tilda_n[1]
+        L_tilda_n254 = L_tilda_n[2]
+        L_tilda_n263 = L_tilda_n[3]
+
+        plt.plot(n_array, L_tilda_n233, c = 'tomato', label = 't/$t_{fb}$ = 1')
+        plt.plot(n_array, L_tilda_n254, c = 'orange', label = 't/$t_{fb}$ = 1.2')
+        plt.plot(n_array, L_tilda_n263, c = 'yellowgreen', label = 't/$t_{fb}$ = 1.3')
+    
+    plt.xlabel(r'$log\nu$ [Hz]')
+    plt.loglog()
+    plt.ylabel(r'$log\tilde{L}_\nu$ [erg/s]')
+    plt.grid()
+    plt.legend()
+    plt.savefig('Ltilda_n_m' + str(m) + '.png' )
+
+
 # plt.axvline(14, color = 'tab:orange')
 # plt.axvline(17, color = 'tab:orange')
 # plt.axvspan(14, 17, alpha=0.3, color = 'tab:orange', label = 'UV band')
-# plt.savefig('TOT_Ltilda_m' + str(m) + '.png' )
 plt.show()
 
--- Nick_plot.py
+++ Nick_plot.py
@@ -1,27 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+days6 = [1, 1.14, 1.3, 1.4]
+L6 = np.loadtxt('L_m6.txt')
 
-n_min = 1e12 
-n_max = 1e19
-n_spacing = 100
-n_array = np.linspace(n_min, n_max, num = n_spacing)
-n_logspace = np.linspace(np.log10(n_min), np.log10(n_max), num = n_spacing)
-
-# CHECK
-# for i in range(1,len(n_array)-1):
-#     print(n_array[i]-n_array[i-1])
-
-# print('------')
-
-# for i in range(1,len(n_array)-1):
-#     print(n_logspace[i]-n_logspace[i-1])
-
-days6 = np.loadtxt('L6.txt')[0]
-L6 = np.loadtxt('L6.txt')[1]
-
-days4 = np.loadtxt('L4.txt')[0]
-L4 = np.loadtxt('L4.txt')[1]
+days4 = [1, 1.2, 1.3]
+L4 = np.loadtxt('L_m4.txt')
 
 plt.plot(days6, L6, 'o-', label = r'$M^6 M_{sun}$')
 plt.plot(days4, L4, 'o-', label = r'$M^4 M_{sun}$')
@@ -30,4 +14,5 @@ plt.xlabel(r'$t/t_{fb}$')
 plt.grid()
 plt.yscale('log')
 plt.legend()
+plt.savefig('Nick.png' )
 plt.show()
