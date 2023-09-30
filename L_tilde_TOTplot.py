77ec49bb14c3003f9c377c0170797ffa672c7e36
spaceturtle707 <p.martire98@gmail.com>
2023-10-01 00:54:23 +0200

    more plot

--- L_tilde_TOTplot.py
+++ L_tilde_TOTplot.py
@@ -1,77 +1,70 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-m = 4
+m = 6
 plot = 'spectra'
-n_min = 1e12 
-n_max = 1e18
-n_spacing = 10000
 
-if plot == 'bolometric' or plot == 'spectra': 
-    L_tilda_n = np.loadtxt('L_tilde_n_m'+ str(m) + '.txt')
+def final_plot(m, pl):
+    L_tilde_n = np.loadtxt('L_tilde_n_m'+ str(m) + '.txt')
     bolom = bolom = np.loadtxt('L_m'+ str(m) + '.txt')
-    n_array = L_tilda_n[0]
+    n_array = L_tilde_n[0]
 
-
-    if plot == 'bolometric':
+    if pl == 'bolometric':
         if m == 6:
             #fixes6 = [844, 881, 925, 950]
             days = [1, 1.14, 1.3, 1.4] #t/t_fb
         if m == 4:
-            #fixes4 = [177, 233,]
-            days = [1, 1.2, 1.3] #t/t_fb
+            #fixes4 = [233, 243, 254, 263]
+            days = [1, 1.1, 1.2, 1.3] #t/t_fb
 
         plt.plot(days, bolom, '-o')
         plt.ylabel(r'$log_{10}$ Luminosity [erg/s]')
         plt.xlabel(r'$t/t_{fb}$')
-        #plt.ylim(1e42,2e45)
-        plt.yscale('log')
+        #plt.yscale('log')
         plt.grid()
         plt.savefig('BolomLtilda_m' + str(m) + '.png' )
 
 
     if plot == 'spectra':
         if m == 6:
-            L_tilda_n844 = L_tilda_n[1]
-            L_tilda_n881 = L_tilda_n[2]
-            L_tilda_n925 = L_tilda_n[3]
-            L_tilda_n950 = L_tilda_n[4]
-            plt.plot(n_array, L_tilda_n844, c = 'tomato', label = 't/$t_{fb}$ = 1')
-            plt.plot(n_array, L_tilda_n881, c = 'orange', label = 't/$t_{fb}$ = 1.14')
-            plt.plot(n_array, L_tilda_n925, c = 'yellowgreen', label = 't/$t_{fb}$ = 1.3')
-            plt.plot(n_array, L_tilda_n950, c = 'teal', label = 't/$t_{fb}$ = 1.4')
-            # plt.plot(n_array, n3)
-            # plt.plot(n_array, n4)
-            #plt.ylim(1e35, 1e45)
+            L_tilde_n844 = L_tilde_n[1]
+            L_tilde_n881 = L_tilde_n[2]
+            L_tilde_n925 = L_tilde_n[3]
+            L_tilde_n950 = L_tilde_n[4]
+            plt.plot(n_array, L_tilde_n844, c = 'tomato', label = 't/$t_{fb}$ = 1')
+            plt.plot(n_array, L_tilde_n881, c = 'orange', label = 't/$t_{fb}$ = 1.14')
+            plt.plot(n_array, L_tilde_n925, c = 'yellowgreen', label = 't/$t_{fb}$ = 1.3')
+            plt.plot(n_array, L_tilde_n950, c = 'teal', label = 't/$t_{fb}$ = 1.4')
 
 
         if m == 4:
-            L_tilda_n233 = L_tilda_n[1]
-            #L_tilda_n254 = L_tilda_n[2]
-            L_tilda_n263 = L_tilda_n[2]
+            L_tilde_n233 = L_tilde_n[1]
+            L_tilde_n243 = L_tilde_n[3]
+            L_tilde_n254 = L_tilde_n[3]
+            L_tilde_n263 = L_tilde_n[3]
 
             times = 0
-            for i in range(len(L_tilda_n233)):
-                if L_tilda_n233[i]>L_tilda_n263[i]:
+            for i in range(len(L_tilde_n233)):
+                if L_tilde_n233[i]>L_tilde_n263[i]:
                     print('the first is bigger ')
                     times +=1
             print(times, 'times')
 
             start = 6500
             stop = 9999
-            plt.plot(10**n_array, L_tilda_n233, c = 'tomato', label = 't/$t_{fb}$ = 1')
-            # #plt.plot(10**n_array, L_tilda_n254, c = 'orange', label = 't/$t_{fb}$ = 1.2')
-            plt.plot(10**n_array, L_tilda_n263, c = 'yellowgreen', label = 't/$t_{fb}$ = 1.3')
-            #plt.plot(10**n_array[start:stop], L_tilda_n233[start:stop], c = 'tomato', label = 't/$t_{fb}$ = 1')
-            #plt.plot(10**n_array[start:stop], L_tilda_n263[start:stop], c = 'yellowgreen', label = 't/$t_{fb}$ = 1.3')
+            plt.plot(10**n_array, L_tilde_n233, c = 'tomato', label = 't/$t_{fb}$ = 1')
+            # #plt.plot(10**n_array, L_tilde_n254, c = 'orange', label = 't/$t_{fb}$ = 1.2')
+            plt.plot(10**n_array, L_tilde_n263, c = 'yellowgreen', label = 't/$t_{fb}$ = 1.3')
+            #plt.plot(10**n_array[start:stop], L_tilde_n233[start:stop], c = 'tomato', label = 't/$t_{fb}$ = 1')
+            #plt.plot(10**n_array[start:stop], L_tilde_n263[start:stop], c = 'yellowgreen', label = 't/$t_{fb}$ = 1.3')
             
-            firstint233 = np.trapz(L_tilda_n233[0:start], n_array[0:start])
-            firstint263 = np.trapz(L_tilda_n263[0:start], n_array[0:start])
+            firstint233 = np.trapz(L_tilde_n233[0:start], n_array[0:start])
+            firstint263 = np.trapz(L_tilde_n263[0:start], n_array[0:start])
             print('first 233: ', firstint233)
             print('first 263: ', firstint263)
 
-            secondint233 = np.trapz(L_tilda_n233[start:stop], n_array[start:stop])
-            secondint263 = np.trapz(L_tilda_n263[start:stop], n_array[start:stop])
+            secondint233 = np.trapz(L_tilde_n233[start:stop], n_array[start:stop])
+            secondint263 = np.trapz(L_tilde_n263[start:stop], n_array[start:stop])
             print('second 233: ', secondint233)
             print('second 263: ', secondint263)
             tot33 = firstint233 + secondint233
@@ -88,8 +81,5 @@ if plot == 'bolometric' or plot == 'spectra':
         #plt.savefig('Ltilda_n_m' + str(m) + '.png' )
 
 
-    # plt.axvline(14, color = 'tab:orange')
-    # plt.axvline(17, color = 'tab:orange')
-    # plt.axvspan(14, 17, alpha=0.3, color = 'tab:orange', label = 'UV band')
     plt.show()
 
--- red.py
+++ red.py
@@ -48,13 +48,14 @@ m = 6 # Choose BH
 Mbh = 10**m 
 Rt =  Mbh**(1/3) # Msol = 1, Rsol = 1
 rg = 2*Mbh/c**2 #tidal radius in simulater units
+
 if m == 4:
-    fixes = np.arange(233,264)
-    days = [1.015, 1.025, 1.0325, 1.0435, 1.0525, 1.06, 1.07, 1.08, 1.0875, 1.0975, 1.1075, 1.115, 1.125, 1.135, 1.1425, 1.1525, 1.1625, 1.17, 1.18, 1.19, 1.1975, 1.2075, 1.2175, 1.2275, 1.235, 1.245, 1.255, 1.2625, 1.2725, 1.2825, 1.29] #t/t_fb
+    fixes = np.concatenate([177], np.arange(233,263+1)) #from 233 to 263
+    days = [0.5, 1.015, 1.025, 1.0325, 1.0435, 1.0525, 1.06, 1.07, 1.08, 1.0875, 1.0975, 1.1075, 1.115, 1.125, 1.135, 1.1425, 1.1525, 1.1625, 1.17, 1.18, 1.19, 1.1975, 1.2075, 1.2175, 1.2275, 1.235, 1.245, 1.255, 1.2625, 1.2725, 1.2825, 1.29] #t/t_fb
     #days = [4.06,4.1,4.13,4.17,4.21,4.24,4.28,4.32,4.35,4.39,4.43,4.46,4.5,4.54,4.57,4.61,4.65,4.68,4.72,4.76,4.79,4.83,4.87,4.91,4.94,4.98,5.02,5.05,5.09,5.13,5.16] #days
 if m == 6:
-    fixes = [844, 881, 925, 950, 1006]
-    days = [1.00325, 1.13975, 1.302, 1.39425, 1.6] #t/t_fb
+    fixes = [844] #[844, 881, 925, 950] # 1006]
+    days = [1.00325, 1.13975, 1.302, 1.39425] # 1.6] #t/t_fb
     #days = [40, 45, 52, 55, 64] #days
 
 #%% Data Load
@@ -255,23 +256,18 @@ def doer_of_thing(fix, m):
     # Turn to luminosity
     sphere_radius *= Rsol_to_cm
     lum = flux * 4 * np.pi * sphere_radius**2
-    print('%.2e' % lum )
+    print('%.7e' % lum )
     return lum
 
 lums = []
 fixesnew = [1006]
-for fix in fixesnew:
+for fix in fixes:
     lum = doer_of_thing(fix, m)
     lums.append(lum)
 
-with open('reddata_m'+ str(m) + '.txt', (days, lums), 'a') as f:
-    f.write(days[4])
-    f.write(lums)
-    f.close()
-
 #%%
 plt.figure()
-np.savetxt('reddata_m'+ str(m) + '.txt', (days, lums))
+np.savetxt('reddatanew_m'+ str(m) + '.txt', (days, lums))
 plt.plot(days, lums, '-o', color = 'maroon')
 plt.yscale('log')
 plt.ylim(1e41,1e45)
@@ -280,3 +276,9 @@ plt.xlabel('Days')
 plt.title('FLD for $10^4 \quad M_\odot$')
 plt.grid()
 plt.show()
+
+#%%
+with open('reddatanew_m'+ str(m) + '.txt', 'a') as f:
+    f.write(days)
+    f.write(lums)
+    f.close()
