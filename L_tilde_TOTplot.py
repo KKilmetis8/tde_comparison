1f4ce3eb381fb054bcd735c1bf0a59df7348f4d5
spaceturtle707 <p.martire98@gmail.com>
2023-10-01 11:17:15 +0200

    plot for Elena

--- L_tilde_TOTplot.py
+++ L_tilde_TOTplot.py
@@ -1,85 +1,129 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-m = 6
-plot = 'spectra'
+# FUNCTIONS
 
-def final_plot(m, pl):
-    L_tilde_n = np.loadtxt('L_tilde_n_m'+ str(m) + '.txt')
-    bolom = bolom = np.loadtxt('L_m'+ str(m) + '.txt')
-    n_array = L_tilde_n[0]
+def select_days(m):
+    if m == 6:
+        #fixes6 = [844, 881, 925, 950]
+        days = [1, 1.14, 1.3, 1.4] #t/t_fb
+    if m == 4:
+        #fixes4 = [233, 243, 254, 263]
+        days = [1, 1.1, 1.2, 1.3] #t/t_fb
+    return days
 
+def final_plot(m, pl, telescope = False, check = False):
     if pl == 'bolometric':
+        days = select_days(m)
+        bolom = bolom = np.loadtxt('L_m'+ str(m) + '.txt')
         if m == 6:
-            #fixes6 = [844, 881, 925, 950]
-            days = [1, 1.14, 1.3, 1.4] #t/t_fb
+            plt.plot(days, bolom, '-o', c = 'olivedrab')
+            plt.text(1, 1e44, '$M_{BH}=10^6M_{sun}$', fontsize = 18)
         if m == 4:
-            #fixes4 = [233, 243, 254, 263]
-            days = [1, 1.1, 1.2, 1.3] #t/t_fb
-
-        plt.plot(days, bolom, '-o')
-        plt.ylabel(r'$log_{10}$ Luminosity [erg/s]')
-        plt.xlabel(r'$t/t_{fb}$')
-        #plt.yscale('log')
+            plt.plot(days, bolom, '-o', c = 'slateblue')
+            plt.text(1, 3.4e41, '$M_{BH}=10^4M_{sun}$', fontsize = 18)
+        plt.yscale('log')
+        plt.ylabel(r'$log_{10}$ Luminosity [erg/s]', fontsize = 18)
+        plt.xlabel(r'$t/t_{fb}$', fontsize = 18)
         plt.grid()
+        plt.legend()
         plt.savefig('BolomLtilda_m' + str(m) + '.png' )
 
+    if pl == 'spectra':
+        L_tilde_n = np.loadtxt('L_tilde_n_m'+ str(m) + '.txt')
+        x_array = L_tilde_n[0]
 
-    if plot == 'spectra':
         if m == 6:
             L_tilde_n844 = L_tilde_n[1]
             L_tilde_n881 = L_tilde_n[2]
             L_tilde_n925 = L_tilde_n[3]
             L_tilde_n950 = L_tilde_n[4]
-            plt.plot(n_array, L_tilde_n844, c = 'tomato', label = 't/$t_{fb}$ = 1')
-            plt.plot(n_array, L_tilde_n881, c = 'orange', label = 't/$t_{fb}$ = 1.14')
-            plt.plot(n_array, L_tilde_n925, c = 'yellowgreen', label = 't/$t_{fb}$ = 1.3')
-            plt.plot(n_array, L_tilde_n950, c = 'teal', label = 't/$t_{fb}$ = 1.4')
-
+            plt.plot(x_array, L_tilde_n844, c = 'tomato', label = 't/$t_{fb}$ = 1')
+            plt.plot(x_array, L_tilde_n881, c = 'orange', label = 't/$t_{fb}$ = 1.14')
+            plt.plot(x_array, L_tilde_n925, c = 'yellowgreen', label = 't/$t_{fb}$ = 1.3')
+            plt.plot(x_array, L_tilde_n950, c = 'teal', label = 't/$t_{fb}$ = 1.4')
+            plt.text(12, 1e27, '$M_{BH}=10^6M_{sun}$', fontsize = 18)
 
         if m == 4:
             L_tilde_n233 = L_tilde_n[1]
-            L_tilde_n243 = L_tilde_n[3]
+            L_tilde_n243 = L_tilde_n[2]
             L_tilde_n254 = L_tilde_n[3]
-            L_tilde_n263 = L_tilde_n[3]
+            L_tilde_n263 = L_tilde_n[4]
 
-            times = 0
-            for i in range(len(L_tilde_n233)):
-                if L_tilde_n233[i]>L_tilde_n263[i]:
-                    print('the first is bigger ')
-                    times +=1
-            print(times, 'times')
+            plt.plot(x_array, L_tilde_n233, c = 'tomato', label = 't/$t_{fb}$ = 1')
+            plt.plot(x_array, L_tilde_n243, c = 'orange', label = 't/$t_{fb}$ = 1.1')
+            plt.plot(x_array, L_tilde_n254, c = 'yellowgreen', label = 't/$t_{fb}$ = 1.2')
+            plt.plot(x_array, L_tilde_n263, c = 'teal', label = 't/$t_{fb}$ = 1.3')
+            plt.text(12, 1e25, '$M_{BH}=10^4M_{sun}$', fontsize = 18)
 
-            start = 6500
-            stop = 9999
-            plt.plot(10**n_array, L_tilde_n233, c = 'tomato', label = 't/$t_{fb}$ = 1')
-            # #plt.plot(10**n_array, L_tilde_n254, c = 'orange', label = 't/$t_{fb}$ = 1.2')
-            plt.plot(10**n_array, L_tilde_n263, c = 'yellowgreen', label = 't/$t_{fb}$ = 1.3')
-            #plt.plot(10**n_array[start:stop], L_tilde_n233[start:stop], c = 'tomato', label = 't/$t_{fb}$ = 1')
-            #plt.plot(10**n_array[start:stop], L_tilde_n263[start:stop], c = 'yellowgreen', label = 't/$t_{fb}$ = 1.3')
-            
-            firstint233 = np.trapz(L_tilde_n233[0:start], n_array[0:start])
-            firstint263 = np.trapz(L_tilde_n263[0:start], n_array[0:start])
-            print('first 233: ', firstint233)
-            print('first 263: ', firstint263)
+            if check:
+                start = 6500
+                stop = 9999
+                # plt.plot(x_array[start:stop], L_tilde_n233[start:stop], c = 'tomato', label = 't/$t_{fb}$ = 1')
+                # plt.plot(x_array[start:stop], L_tilde_n263[start:stop], c = 'yellowgreen', label = 't/$t_{fb}$ = 1.3')
+                
+                firstint233 = np.trapz(L_tilde_n233[0:start], x_array[0:start])
+                firstint263 = np.trapz(L_tilde_n263[0:start], x_array[0:start])
+                print('first 233: ', firstint233)
+                print('first 263: ', firstint263)
 
-            secondint233 = np.trapz(L_tilde_n233[start:stop], n_array[start:stop])
-            secondint263 = np.trapz(L_tilde_n263[start:stop], n_array[start:stop])
-            print('second 233: ', secondint233)
-            print('second 263: ', secondint263)
-            tot33 = firstint233 + secondint233
-            tot263 = firstint263 + secondint263
-            print('233: ', tot33)
-            print('263: ', tot263)
+                secondint233 = np.trapz(L_tilde_n233[start:stop], x_array[start:stop])
+                secondint263 = np.trapz(L_tilde_n263[start:stop], x_array[start:stop])
+                print('second 233: ', secondint233)
+                print('second 263: ', secondint263)
+                tot33 = firstint233 + secondint233
+                tot263 = firstint263 + secondint263
+                print('233: ', tot33)
+                print('263: ', tot263)
 
-        
-        plt.xlabel(r'$log\nu$ [Hz]')
-        plt.loglog()
-        plt.ylabel(r'$log\tilde{L}_\nu$ [erg/s]')
+        plt.xlabel(r'$log_{10}\nu$ [Hz]', fontsize = 18)
+        plt.yscale('log')
+        plt.ylabel(r'$log_{10}\tilde{L}_\nu$ [erg/s]', fontsize = 18)
+        plt.ylabel(r'$log_{10}(\tilde{L}_\nu/L)$ [erg/s]', fontsize = 18)
         plt.grid()
-        plt.legend()
-        #plt.savefig('Ltilda_n_m' + str(m) + '.png' )
 
+        if telescope == False:
+            plt.legend()
+            plt.savefig('spectra_m' + str(m) + '.png' )
 
-    plt.show()
+            #If you want the plot without logscale
+            # plt.xlim(13,17)
+            # plt.ylabel(r'$\tilde{L}_\nu$ [erg/s]', fontsize = 18)
+            # plt.grid()
+            # plt.savefig('NOlog_spectra_m' + str(m) + '.png' )
 
+        if telescope: 
+            ultrasat_min = 1.03e15
+            ultrasat_max = 1.3e15
+            r_min = 4.11e14
+            r_max = 5.07e14
+            g_min = 5.66e14
+            g_max = 7.48e14
+
+            plt.xlim(14,16)
+            plt.ylim(1e22,1e30)
+            plt.axvline(np.log10(ultrasat_min), color = 'b')
+            plt.axvline(np.log10(ultrasat_max), color = 'b')
+            plt.axvspan(np.log10(ultrasat_min), np.log10(ultrasat_max), alpha=0.4, color = 'b')
+            plt.text(np.log10(ultrasat_min)+0.04,1e23,'ULTRASAT', rotation = 90)
+
+            plt.axvline(np.log10(r_min), color = 'r')
+            plt.axvline(np.log10(r_max), color = 'r')
+            plt.axvspan(np.log10(r_min), np.log10(r_max), alpha=0.4, color = 'r')
+            plt.text(np.log10(r_min)+0.04,1e23,'R-band ZTF', rotation = 90)
+
+            plt.axvline(np.log10(g_min), color = 'orange')
+            plt.axvline(np.log10(g_max), color = 'orange')
+            plt.axvspan(np.log10(g_min), np.log10(g_max), alpha=0.4, color = 'orange')
+            plt.text(np.log10(g_min)+0.05,1e23,'G-band ZTF', rotation = 90)
+            plt.legend()
+            plt.savefig('telescope_spectra_m' + str(m) + '.png' )
+
+# MAIN
+m = 6
+
+plt.figure(figsize=(15,8))
+plotbolo = final_plot(m,'bolometric')
+plt.figure(figsize=(15,8))
+plotspectra = final_plot(m,'spectra')
+plt.show()
--- Nick_plot.py
+++ Nick_plot.py
@@ -4,15 +4,16 @@ import matplotlib.pyplot as plt
 days6 = [1, 1.14, 1.3, 1.4]
 L6 = np.loadtxt('L_m6.txt')
 
-days4 = [1, 1.2, 1.3]
+days4 = [1, 1.1, 1.2, 1.3]
 L4 = np.loadtxt('L_m4.txt')
 
-plt.plot(days6, L6, 'o-', label = r'$M^6 M_{sun}$')
-plt.plot(days4, L4, 'o-', label = r'$M^4 M_{sun}$')
+plt.figure(figsize=(15,8))
+plt.plot(days6, L6, 'o-', label = r'$M_{BH}=10^6M_{sun}$', c = 'olivedrab')
+plt.plot(days4, L4, 'o-', label = r'$M_{BH}=10^4M_{sun}$', c = 'slateblue')
 plt.ylabel(r'$log_{10}$ Luminosity [erg/s]')
 plt.xlabel(r'$t/t_{fb}$')
 plt.grid()
 plt.yscale('log')
 plt.legend()
-plt.savefig('Nick.png' )
+plt.savefig('Bolom46.png')
 plt.show()
