45aba261850f50bdac854ed33ad94a0f231e4efc
KKilmetis8 <asansol2552@gmail.com>
2023-10-12 20:44:51 +0200

    added extrapolation.  Removed data from gitignore. You can find Elad's data there

--- src/Optical_Depth/opacity_table.py
+++ src/Optical_Depth/opacity_table.py
@@ -11,6 +11,7 @@ NOTES FOR OTHERS
 
 import numpy as np
 from scipy.interpolate import RegularGridInterpolator
+from scipy.interpolate import CubicSpline
 
 # All units are ln[cgs]
 loadpath = 'src/Optical_Depth/'
@@ -23,6 +24,7 @@ lnk_scatter = np.loadtxt(loadpath + 'scatter.txt')
 lnk_scatter_inter = RegularGridInterpolator( (lnT, lnrho), lnk_scatter)
 lnk_ross_inter = RegularGridInterpolator( (lnT, lnrho), lnk_ross)
 lnk_planck_inter = RegularGridInterpolator( (lnT, lnrho), lnk_planck)
+
 
 def opacity(T, rho, kind, ln = True) -> float:
     '''
@@ -53,26 +55,78 @@ def opacity(T, rho, kind, ln = True) -> float:
         # Remove fuckery
         T = np.nan_to_num(T, nan = 0, posinf = 0, neginf= 0)
         rho = np.nan_to_num(rho, nan = 0, posinf = 0, neginf= 0)
-
-    # Pick Opacity & Use Interpolation Function
-    if kind == 'rosseland':
-        ln_opacity = lnk_ross_inter((T, rho))
-        
-    elif kind == 'planck':
-        ln_opacity = lnk_planck_inter((T, rho))
-        
-    elif kind == 'effective':
-        planck = lnk_planck_inter((T, rho))
-        scattering = lnk_scatter_inter((T, rho))
-        
-        # Rybicky & Lightman eq. 1.98
-        ln_opacity = np.sqrt(planck * (planck + scattering)) 
+    print(rho)
+    if rho > -23:
+        # Pick Opacity & Use Interpolation Function
+        if kind == 'rosseland':
+            ln_opacity = lnk_ross_inter((T, rho))
+            
+        elif kind == 'planck':
+            ln_opacity = lnk_planck_inter((T, rho))
+            
+        elif kind == 'effective':
+            planck = lnk_planck_inter((T, rho))
+            scattering = lnk_scatter_inter((T, rho))
+            
+            # Rybicky & Lightman eq. 1.98 NO USE STEINGERG & STONE (9)
+            ln_opacity = np.sqrt(3 * planck * (planck + scattering)) 
+        elif kind == 'red':
+            planck = lnk_planck_inter((T, rho))
+            scattering = lnk_scatter_inter((T, rho))
+            
+            # Rybicky & Lightman eq. 1.98 NO USE STEINGERG & STONE (9)
+            ln_opacity = planck + scattering
+        else:
+            print('Invalid opacity type. Try: rosseland / planck / effective.')
+            return 1
     else:
-        print('Invalid opacity type. Try: rosseland / planck / effective.')
-        return 1
+        # NOTE: Fair chance that this is VERY slow
+        # Maybe do all 128x3 cubic splines at the start?
+        # We could just straight up replace RegularGridInterpolator
+        if kind == 'rosseland':
+            # Get the column
+            idx = np.argmin( np.abs( T - lnT ))
+            opacity_col = lnk_ross[idx]
+            
+            # Inter/Extra polate
+            extra = CubicSpline(lnrho, opacity_col, bc_type='natural')
+            ln_opacity = extra(rho)
+        
+        elif kind == 'planck':
+            # Get the column
+            idx = np.argmin( np.abs( T - lnT ))
+            opacity_col = lnk_planck[idx]
+            
+            # Inter/Extra polate
+            extra = CubicSpline(lnrho, opacity_col, bc_type='natural')
+            ln_opacity = extra(rho)       
+            
+        elif kind == 'effective':
+            # Get the column
+            idx = np.argmin( np.abs( T - lnT ))
+            
+            # sqrt(3 plank (plank + scattering) )s
+            opacity_col = np.add(lnk_planck[idx], lnk_scatter[idx])
+            opacity_col = np.multiply(3 * opacity_col, lnk_planck[idx])
+            opacity_col = np.sqrt(opacity_col)
+            # Inter/Extra polate
+            extra = CubicSpline(lnrho, opacity_col, bc_type='natural')
+            ln_opacity = extra(rho)
+            
+        elif kind == 'red':
+            idx = np.argmin( np.abs( T - lnT ))
+            opacity_col = np.add(lnk_planck[idx], lnk_scatter[idx])
+            
+            # Inter/Extra polate
+            extra = CubicSpline(lnrho, opacity_col, bc_type='natural')
+            ln_opacity = extra(rho)
             
     # Remove the ln
     opacity = np.exp(ln_opacity)
     
     return opacity
+
+if __name__ == '__main__':
+    opa = opacity(1e9, 1e-11, 'planck', ln = False)
+    print(opa)
     
