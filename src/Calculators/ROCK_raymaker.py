96c9de25eb65f916311dc32bb610791763d6f4a9
KKilmetis8 <asansol2552@gmail.com>
2023-11-07 17:32:21 +0100

    tubes seem to be working (small hope)

--- src/Calculators/ROCK_raymaker.py
+++ src/Calculators/ROCK_raymaker.py
@@ -11,7 +11,9 @@ import healpy as hp
 import matplotlib.pyplot as plt
 from astropy.coordinates import cartesian_to_spherical
 from src.Opacity.opacity_table import opacity
+import numba
 
+# Constants
 NSIDE = 4
 G = 6.6743e-11 # SI
 Msol = 1.98847e30 # kg
@@ -24,15 +26,16 @@ Rsol_to_cm = 6.957e10 # [cm]
 den_converter = Msol_to_g / Rsol_to_cm**3
 en_den_converter = Msol_to_g / (Rsol_to_cm  * t**2 ) # Energy Density converter
 
-fix = '881'
+fix = '844'
 m = 6
-X = np.load( str(m) + '/'  + fix + '/CMx_' + fix + '.npy')
-Y = np.load( str(m) + '/'  + fix + '/CMy_' + fix + '.npy')
-Z = np.load( str(m) + '/'  + fix + '/CMz_' + fix + '.npy')
-Mass = np.load( str(m) + '/'  + fix + '/Mass_' + fix + '.npy')
-T = np.load( str(m) + '/'  + fix + '/T_' + fix + '.npy')
-Den = np.load( str(m) + '/'  + fix + '/Den_' + fix + '.npy')
-Rad = np.load( str(m) + '/'  +fix + '/Rad_' + fix + '.npy')
+prune = 1
+X = np.load( str(m) + '/'  + fix + '/CMx_' + fix + '.npy')[::prune]
+Y = np.load( str(m) + '/'  + fix + '/CMy_' + fix + '.npy')[::prune]
+Z = np.load( str(m) + '/'  + fix + '/CMz_' + fix + '.npy')[::prune]
+Mass = np.load( str(m) + '/'  + fix + '/Mass_' + fix + '.npy')[::prune]
+T = np.load( str(m) + '/'  + fix + '/T_' + fix + '.npy')[::prune]
+Den = np.load( str(m) + '/'  + fix + '/Den_' + fix + '.npy')[::prune]
+Rad = np.load( str(m) + '/'  +fix + '/Rad_' + fix + '.npy')[::prune]
 
 Rad *= Den 
 Rad *= en_den_converter
@@ -51,31 +54,37 @@ delta_thetas = []
 delta_phis = []
 fig, ax = plt.subplots(1,1, subplot_kw=dict(projection="mollweide"))
 for i in range(192):
-    theta_diff = 0
-    phi_diff = 0
-    
+    # Get Observers
     thetas[i], phis[i] = hp.pix2ang(NSIDE, i)
     thetas[i] -= np.pi/2 # Enforce theta in -pi to pi    
     observers.append( (thetas[i], phis[i]) )
     
+    # Get their opening angles
+    theta_diff = 0
+    phi_diff = 0
     corner_angles = []
+    # Corner XYZ coordinates
     corner_x, corner_y, corner_z = hp.boundaries(4,i)
     corners = np.zeros((4,3))
     corner_angles = np.zeros((4,2))
     
+    # Corner 
     for i in range(4):
         corners[i] = corner_x[i], corner_y[i], corner_z[i]
         corner_angles[i][0] = hp.vec2ang(corners[i])[0][0]
         corner_angles[i][1] = hp.vec2ang(corners[i])[1][0]
         
-    # print(corner_angles.T[1].max() - corner_angles.T[1].min())
-    # corner_angles_phi_plot = list(corner_angles.T[1] - np.pi)
-    # corner_angles_phi_plot.append(corner_angles_phi_plot[0])
-    # corner_angles_theta_plot = list(corner_angles.T[0] - np.pi/2)
-    # corner_angles_theta_plot.append(corner_angles_theta_plot[0])
+    # Biggest possible Delta phi and Delta theta
+    print(corner_angles.T[1].max() - corner_angles.T[1].min())
+    corner_angles_phi_plot = list(corner_angles.T[1] - np.pi)
+    corner_angles_theta_plot = list(corner_angles.T[0] - np.pi/2)
     
-    # plt.plot(corner_angles_phi_plot, corner_angles_theta_plot, '-x', 
-    #           markersize = 1, c='k', linewidth = 1, zorder = 4,)
+    # last point to close the rectangle
+    corner_angles_phi_plot.append(corner_angles_phi_plot[0]) 
+    corner_angles_theta_plot.append(corner_angles_theta_plot[0])
+    
+    plt.plot(corner_angles_phi_plot, corner_angles_theta_plot, '-x', 
+              markersize = 1, c='k', linewidth = 1, zorder = 4,)
 
     for i in range(0,4):
         theta_diff_temp = np.max(np.abs(corner_angles[i][0] - corner_angles[:][0]))
@@ -89,7 +98,7 @@ for i in range(192):
             
     delta_thetas.append(theta_diff/2)
     delta_phis.append(phi_diff/2)
-# plt.plot(PHI[::100] - np.pi, THETA[::100], 'x',  c = 'r', markersize = 0.1, zorder = 2) 
+plt.plot(PHI[::100] - np.pi, THETA[::100], 'x',  c = 'r', markersize = 0.1, zorder = 2) 
 
 
 #%% Make rays
@@ -150,7 +159,8 @@ def get_kappa(T: float, rho: float, dr: float):
     # Too hot: Kramers' law for absorption (planck)
     if T > np.exp(17.876): # 58_002_693
         X = 0.7389
-        kplanck = 3.68 * 1e22 * (1 + X) * T**(-3.5) * rho #Kramers' opacity [cm^2/g]
+        Z = 0.02
+        kplanck =  1.2e26 * Z * (1 + X) * T**(-3.5) * rho #Kramers' bound-free opacity [cm^2/g]
         kplanck *= rho
 
         Tscatter = np.exp(17.87)
@@ -184,7 +194,6 @@ def calc_photosphere(T, rho, rs):
         kappas.append(new_kappa)
         cumulative_kappas.append(kappa)
         i -= 1
-
     photo =  rs[i] #i it's negative
     return kappas, cumulative_kappas, photo
 
@@ -207,7 +216,7 @@ def get_photosphere(rays_T, rays_den, rays_R):
     rays_photo = np.zeros(len(rays_T))
     
     for i in range(len(rays_T)):
-        print('ray: ', i) 
+        print('Ray maker ray: ', i) 
         # Isolate each ray
         T_of_single_ray = rays_T[i]
         Den_of_single_ray = rays_den[i]
@@ -224,5 +233,161 @@ def get_photosphere(rays_T, rays_den, rays_R):
 
     return rays_cumulative_kappas, rays_photo
 
-stop = 70
-cumul_kappa, photo =  get_photosphere(rays_T[:70], rays_Den[:70], rays_R[:70])
+stop = 192
+cumul_kappa, photo =  get_photosphere(rays_T[:stop], rays_Den[:stop], rays_R[:stop])
+#%%
+
+@numba.njit
+def grad_calculator(ray: np.array, radii: np.array, r_photo): 
+    # For a single ray (in logspace) get 
+    # the index of radius closest to sphere_radius and the gradE there.
+    # Everything is in CGS.
+    for i, radius in enumerate(radii):
+        if radius > r_photo:
+            idx = i - 1 
+            break
+    
+    # For rad
+    if idx<0:
+        print('Bad Observer, photosphere is the closest point')
+        idx=0
+        
+    step = radii[idx+1] - radii[idx]
+    grad_E = (ray[idx+1] - ray[idx]) / step 
+
+    return grad_E, idx
+
+    
+def flux_calculator(grad_E, idx, 
+                    single_Rad, single_T, single_Den):
+    """
+    Get the flux for every observer.
+    Eevrything is in CGS.
+
+    Parameters: 
+    grad_E idx_tot are 1D-array of lenght = len(rays)
+    rays, rays_T, rays_den are len(rays) x N_cells arrays
+    """
+    # We compute stuff OUTSIDE the photosphere
+    # (which is at index idx_tot[i])
+    idx = idx+1 #  
+    Energy = single_Rad[idx]
+    max_travel = np.sign(-grad_E) * c_cgs * Energy # or should we keep the abs???
+    
+    Temperature = single_T[idx]
+    Density = single_Den[idx]
+    
+    # Ensure we can interpolate
+    rho_low = np.exp(-45)
+    T_low = np.exp(8.77)
+    T_high = np.exp(17.8)
+    
+    # If here is nothing, light continues
+    if Density < rho_low:
+        return max_travel
+    
+    # If stream, no light 
+    if Temperature < T_low: 
+        return 0
+    
+    # T too high => Kramers'law
+    if Temperature > T_high:
+        X = 0.7389
+        k_ross = 3.68 * 1e22 * (1 + X) * Temperature**(-3.5) * Density #Kramers' opacity [cm^2/g]
+        k_ross *= Density
+    else:    
+        # Get Opacity, NOTE: Breaks Numba
+        k_ross = opacity(Temperature, Density, 'rosseland', ln = False)
+    
+    # Calc R, eq. 28
+    R = np.abs(grad_E) /  (k_ross * Energy)
+    invR = 1 / R
+    # Calc lambda, eq. 27
+    coth = 1 / np.tanh(R)
+    lamda = invR * (coth - invR)
+    # Calc Flux, eq. 26
+    Flux = - c_cgs * grad_E  * lamda / k_ross
+    
+    # Take the minimum between F, cE
+    if Flux > max_travel:
+        return max_travel
+    else:
+        return Flux
+
+fluxes = []
+for i in range(len(rays_T)):
+    print('Flux ray: ', i) 
+
+    # Isolate ray
+    single_T = rays_T[i]
+    single_Den = rays_Den[i]
+    single_Rad = rays_Rad[i]
+    single_radii = rays_R[i]
+    
+    #  Calculate gradE
+    grad_E, idx = grad_calculator(single_Rad, single_radii, photo[i])
+
+    # Calculate Flux 
+    flux = flux_calculator(grad_E, idx, 
+                            single_Rad, single_T, single_Den)
+    fluxes.append(flux) # Keep it
+    
+#%% Convert to luminosity 
+lum = np.zeros(len(fluxes))
+for i in range(len(fluxes)):
+    # Ignore negative flux
+    if fluxes[i] < 0:
+        continue
+    # Convert photo to cm
+    r = photo[i] * Rsol_to_cm
+    lum[i] = fluxes[i] * 4 * np.pi * r**2
+
+# Average in observers
+bol_lum = np.sum(lum)/192
+
+from src.Utilities.finished import finished
+finished()
+#%% Plot & Print
+plt.rcParams['text.usetex'] = True
+plt.rcParams['figure.dpi'] = 300
+plt.rcParams['font.family'] = 'Times New Roman'
+plt.rcParams['figure.figsize'] = [5 , 4]
+plt.rcParams['axes.facecolor']= 	'whitesmoke'
+plt.rcParams['figure.figsize'] = [5 , 4]
+plt.rcParams['xtick.direction'] = 'in'
+plt.rcParams['ytick.direction'] = 'in'
+AEK = '#F1C410' # Important color 
+
+# Print bol lum
+print('---')
+print('L FLD: %.2e' % bol_lum)
+print('Should be: 3e42')
+
+# Plot
+fig, ax = plt.subplots(1,2, figsize = (10, 6))
+ax[0].plot(lum, 'o', c ='k')
+
+# Make pretty
+ax[0].set_yscale('log')
+ax[0].set_title('FLD Luminosity')
+ax[0].set_xlabel('Observers')
+ax[0].set_ylabel('Luminosity [erg/s]')
+
+# Plot Photo
+ax[1].plot(photo, 'o', c = 'k')
+
+## Make pretty
+# My lines
+from scipy.stats import gmean
+ax[1].axhline(np.mean(photo), linestyle = '--' ,c = 'seagreen')
+ax[1].axhline(gmean(photo), linestyle = '--', c = 'darkorange')
+#ax[1].text()
+# Elad's lines
+ax[1].axhline(40 , c = AEK)
+ax[1].axhline(700, c= 'lightseagreen')
+
+ax[1].set_yscale('log')
+ax[1].set_title('Photosphere')
+ax[1].set_ylabel(r'Photosphere Radius [$R_\odot$]')
+ax[1].set_xlabel('Observers')
+
