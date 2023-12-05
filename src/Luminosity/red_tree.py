62b60b12e0f1052fac29d46759fca2671d14abc8
KKilmetis8 <asansol2552@gmail.com>
2023-12-05 15:53:06 +0100

    dumb bug #3

--- src/Luminosity/red_tree.py
+++ src/Luminosity/red_tree.py
@@ -55,6 +55,7 @@ def select_fix(m, check = 'fid'):
             snapshots = np.arange(100, 344 + 1)
         if m == 4 and check == 'S60ComptonHires':
             snapshots = np.arange(210, 271 + 1)
+        days = []
     else:
         if m == 4:
             snapshots = [233] #, 254, 263, 277 , 293, 308, 322]
