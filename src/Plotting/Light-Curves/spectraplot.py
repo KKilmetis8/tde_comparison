5eb037a2d3ad639047c48925f5e6dc16bb056e35
spaceturtle707 <p.martire98@gmail.com>
2023-12-30 10:13:45 +0100

    deploy special radii on alice

--- src/Plotting/Light-Curves/spectraplot.py
+++ src/Plotting/Light-Curves/spectraplot.py
@@ -8,7 +8,7 @@ import sys
 sys.path.append('/Users/paolamartire/tde_comparison')
 
 m = 6
-snap = 952
+snap = 881
 axis = 'temp'
 
 c = 2.99792458e10 #[cm/s]
@@ -22,7 +22,7 @@ def wavelength(n):
         # in angststrom 
         return c *1e8 / n 
 
-nL_tilde_n = np.loadtxt(f'data/blue/nLn_single_m{m}_{snap}.txt')
+nL_tilde_n = np.loadtxt(f'data/blue/nLn_single_m{m}_{snap}_all.txt')
 x_array = np.loadtxt(f'data/blue/spectrafreq_m{m}.txt')
 n_array = np.power(10, x_array)
 n_start = 1e13
@@ -41,8 +41,9 @@ if axis == 'temp':
         x_end = temperature(n_end)
 
 fig, ax1 = plt.subplots( figsize = (6,6) )
-ax1.plot(x_axis, n_array * nL_tilde_n[0], c = 'b', label = r'$\vec{x}$')
-ax1.plot(x_axis, n_array * nL_tilde_n[1], c = 'r', label = r'$-\vec{x}$')
+for i in range(1,len(nL_tilde_n), 15):
+        ax1.plot(x_axis, n_array * nL_tilde_n[i], label = f'observer {i}')#, c = 'b', label = r'$\vec{x}$')
+# ax1.plot(x_axis, n_array * nL_tilde_n[1], c = 'r', label = r'$-\vec{x}$')
 # ax1.plot(x_axis, n_array * nL_tilde_n[2], c = 'k', label = r'$\vec{y}$')
 # ax1.plot(x_axis, n_array * nL_tilde_n[3], c = 'lime', label = r'$-\vec{y}$')
 # ax1.plot(x_axis, n_array * nL_tilde_n[4], c = 'magenta', label = r'$\vec{z}$')
@@ -61,7 +62,7 @@ ax2.invert_xaxis()
 ax2.loglog()
 ax2.set_xlabel(r'$log_{10}\lambda [\AA]$')
 ax1.legend()
-plt.savefig(f'Figs/singlespectra{snap}')
+#plt.savefig(f'Figs/singlespectra{snap}')
 plt.show()
 
 
