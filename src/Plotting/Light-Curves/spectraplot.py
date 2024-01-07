correct normalisation

src/Plotting/Light-Curves/spectraplot.py
@@ -8,7 +8,7 @@ import sys
 sys.path.append('/Users/paolamartire/tde_comparison')
 
 m = 6
-snap = 881
+snap = 1008
 axis = 'temp'
 
 c = 2.99792458e10 #[cm/s]
@@ -22,7 +22,8 @@ def wavelength(n):
         # in angststrom 
         return c *1e8 / n 
 
-nL_tilde_n = np.loadtxt(f'data/blue/nLn_single_m6_881.txt')
+nL_tilde_n = np.loadtxt(f'data/blue/nLn_single_m{m}_{snap}.txt')
+nL_tilde_n*= 1.6339520760162161e+44/4.6809232785802375e+42
 x_array = np.loadtxt(f'data/blue/spectrafreq_m{m}.txt')
 n_array = np.power(10, x_array)
 n_start = 1e13
@@ -43,10 +44,10 @@ if axis == 'temp':
 fig, ax1 = plt.subplots( figsize = (6,6) ) 
 ax1.plot(x_axis, n_array * nL_tilde_n[0], c = 'b', label = r'$\vec{x}$')
 ax1.plot(x_axis, n_array * nL_tilde_n[1], c = 'r', label = r'$-\vec{x}$')
-# ax1.plot(x_axis, n_array * nL_tilde_n[2], c = 'k', label = r'$\vec{y}$')
-# ax1.plot(x_axis, n_array * nL_tilde_n[3], c = 'lime', label = r'$-\vec{y}$')
-# ax1.plot(x_axis, n_array * nL_tilde_n[4], c = 'magenta', label = r'$\vec{z}$')
-# ax1.plot(x_axis, n_array * nL_tilde_n[5], c = 'aqua', label = r'$-\vec{z}$')
+ax1.plot(x_axis, n_array * nL_tilde_n[2], c = 'k', label = r'$\vec{y}$')
+ax1.plot(x_axis, n_array * nL_tilde_n[3], c = 'lime', label = r'$-\vec{y}$')
+ax1.plot(x_axis, n_array * nL_tilde_n[4], c = 'magenta', label = r'$\vec{z}$')
+ax1.plot(x_axis, n_array * nL_tilde_n[5], c = 'aqua', label = r'$-\vec{z}$')
 ax2 = ax1.twiny()
 ax1.set_xlabel(f'{label}')
 ax1.set_ylabel(r'$log_{10}(\nu L_\nu)$ [erg/s]')
@@ -61,7 +62,7 @@ ax2.invert_xaxis()
 ax2.loglog()
 ax2.set_xlabel(r'$log_{10}\lambda [\AA]$')
 ax1.legend()
-#plt.savefig(f'Figs/singlespectra{snap}')
+plt.savefig(f'Figs/singlespectra{snap}')
 plt.show()
 
 