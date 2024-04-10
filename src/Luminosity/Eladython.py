c2b4a9158ca5b3f40f59a848efd43bcc6811d07b
KKilmetis8 <asansol2552@gmail.com>
2024-04-10 11:20:27 +0200

    eladython works

--- src/Luminosity/Eladython.py
+++ src/Luminosity/Eladython.py
@@ -37,6 +37,7 @@ box = np.zeros(6)
 with h5py.File(f'{pre}{fix}/snap_{fix}.h5', 'r') as fileh:
     for i in range(len(box)):
         box[i] = fileh['Box'][i]
+    fileh.close()
         
 Rad_den = np.multiply(Rad,Den)
 R = np.sqrt(X**2 + Y**2 + Z**2)
@@ -45,17 +46,17 @@ velocity = np.sqrt(VX**2 + VY**2 + VZ**2)
 #%% Cross dot -----------------------------------------------------------------
 import healpy as hp
 observers_xyz = hp.pix2vec(c.NSIDE, range(192))
-observers_xyz = np.array([observers_xyz])
-observers_xyz = np.reshape(observers_xyz, (192,3))
+# observers_xyz = np.array([observers_xyz])
+# observers_xyz = np.reshape(observers_xyz, (192,3))
 # Line 17, * is matrix multiplication, ' is .T
-cross_dot = np.matmul(observers_xyz,  observers_xyz.T )
-cross_dot[cross_dot<0] = 0
-cross_dot *= 4/192
+# cross_dot = np.matmul(observers_xyz,  observers_xyz.T )
+# cross_dot[cross_dot<0] = 0
+# cross_dot *= 4/192
 
 #%% Opacities -----------------------------------------------------------------
 # Freq range
 f_min = c.Kb * 1e3 / c.h
-f_max = c.Kb * 1e13 / c.h
+f_max = c.Kb * 3e13 / c.h
 f_num = 1_000
 frequencies = np.logspace(np.log10(f_min), np.log10(f_max), f_num)
 
@@ -105,10 +106,12 @@ T_cool2, Rho_cool2, rossland2 = pad_interp(T_cool, Rho_cool, rossland.T)
 _, _, plank2 = pad_interp(T_cool, Rho_cool, plank.T)
 
 #%% Tree ----------------------------------------------------------------------
-
+import matlab.engine
+eng = matlab.engine.start_matlab()
 from scipy.spatial import KDTree
 xyz = np.array([X, Y, Z]).T
-tree = KDTree(xyz)
+# tree = KDTree(xyz, leafsize=50)
+tree = eng.KDTreeSearcher(xyz)
 N_ray = 5_000
 
 # In the future these will be ray class
@@ -158,28 +161,36 @@ F_photo_temp = np.zeros((192, f_num))
 
 # Dynamic Box -----------------------------------------------------------------
 #for i in range(1):
-i = 0
+i = 74
 # Progress 
 if i % 10 == 0:
     print('Eladython Ray no:', i)
     
-mu_x = observers_xyz[i][0]
-mu_y = observers_xyz[i][1]
-mu_z = observers_xyz[i][2]
+mu_x = observers_xyz[0][i]
+mu_y = observers_xyz[1][i]
+mu_z = observers_xyz[2][i]
 
 # Box is for dynamic ray making
 if mu_x < 0:
     rmax = box[0] / mu_x
+    print('x-', rmax)
 else:
     rmax = box[3] / mu_x
+    print('x+', rmax)
 if mu_y < 0:
     rmax = min(rmax, box[1] / mu_y)
+    print('y-', rmax)
 else:
     rmax = min(rmax, box[4] / mu_y)
+    print('y+', rmax)
+
 if mu_z < 0:
     rmax = min(rmax, box[2] / mu_z)
+    print('z-', rmax)
 else:
     rmax = min(rmax, box[5] / mu_z)
+    print('z+', rmax)
+print(rmax)
 # This naming is so bad
 r = np.logspace( -0.25, np.log10(rmax), N_ray)
 alpha = (r[1] - r[0]) / (0.5 * ( r[0] + r[1]))
@@ -189,32 +200,34 @@ x = r*mu_x
 y = r*mu_y
 z = r*mu_z
 xyz2 = np.array([x, y, z]).T
-_ , idx = tree.query(xyz2)
+# _ , idx = tree.query(xyz2)
+idx = eng.knnsearch(tree, xyz2)
+idx = [ int(idx[i][0] - 1) for i in range(len(idx))] # -1 because we start from 0
 d = Den[idx] * c.den_converter
 t = T[idx]
-# i honest to goodness do not understand why we interpolate for a second time
-# i think we dont actually do it twice
-from scipy.interpolate import  griddata
 
-# sigma_rossland = RectBivariateSpline(x = T_cool, y = Rho_cool, z = rossland)
-# sigma_rossland_eval = np.exp(sigma_rossland(np.log(t), np.log(d), grid = False))
-# sigma_plank = RectBivariateSpline(x = T_cool, y = Rho_cool, z = plank)
-# sigma_plank_eval = np.exp(sigma_plank(np.log(t), np.log(d), grid = False))
+#%% Interpolate
+from scipy.interpolate import griddata
+# T_cool2, Rho_cool2 = np.meshgrid(T_cool2,Rho_cool2)
+# T_cool2 = T_cool2.ravel()
 
-T_cool2, Rho_cool2 = np.meshgrid(T_cool2,Rho_cool2)
-T_cool2 = T_cool2.ravel()
+# Rho_cool2 = Rho_cool2.ravel()
 
-Rho_cool2 = Rho_cool2.ravel()
-
-cool = np.array([T_cool2, Rho_cool2]).T
-# sigma_rossland = griddata( (T_cool2, Rho_cool2), rossland2.ravel(),
+# cool = np.array([T_cool2, Rho_cool2]).T
+# sigma_rossland = griddata( cool, rossland2.ravel(),
 #                             xi = np.array([np.log(t), np.log(d)]).T )
-sigma_rossland = griddata( cool, rossland2.ravel(),
-                            xi = np.array([np.log(t), np.log(d)]).T )
-sigma_plank = griddata( (T_cool2, Rho_cool2), plank2.ravel(),
-                            xi = np.array([np.log(t), np.log(d)]).T )
+# sigma_plank = griddata( (T_cool2, Rho_cool2), plank2.ravel(),
+#                             xi = np.array([np.log(t), np.log(d)]).T )
 
-sigma_rossland_eval = np.exp(sigma_rossland)
+# sigma_rossland_eval = np.exp(sigma_rossland)
+# sigma_plank_eval = np.exp(sigma_plank)
+
+sigma_rossland = eng.interp2(T_cool2,Rho_cool2,rossland2,np.log(t),np.log(d),'linear',0)
+sigma_rossland = [sigma_rossland[0][i] for i in range(N_ray)]
+sigma_rossland_eval = np.exp(sigma_rossland) 
+
+sigma_plank = eng.interp2(T_cool2,Rho_cool2,plank2,np.log(t),np.log(d),'linear',0)
+sigma_plank = [sigma_plank[0][i] for i in range(N_ray)]
 sigma_plank_eval = np.exp(sigma_plank)
 #%% Optical Depth ---------------------------------------------------------------
 # Okay, line 232, this is the hard one.
@@ -236,10 +249,13 @@ los_effective = - np.flipud(sci.cumulative_trapezoid(k_effective, r_fuT, initial
 
 # Get 20 unique, nearest neighbors
 xyz3 = np.array([X[idx], Y[idx], Z[idx]]).T
-_, idxnew = tree.query(xyz3, k=20) # 20 nearest neighbors
+# _, idxnew = tree.query(xyz3, k=20) # 20 nearest neighbors
+idxnew = eng.knnsearch(tree, xyz3, 'K', 20)
+#
+
 #idxnew = np.array([idxnew], dtype = int).T #np.reshape(idxnew, (1, len(idxnew))) #.T
 idxnew = np.unique(idxnew).T
-
+idxnew = [ int(idxnew[i] -1) for i in range(len(idxnew))]
 # Cell radius
 dx = 0.5 * Vol[idx]**(1/3)
 
@@ -255,12 +271,15 @@ gradx_p = griddata( f_inter_input, Rad_den[idxnew], method = 'linear',
 gradx_m = griddata( f_inter_input, Rad_den[idxnew], method = 'linear',
                     xi = np.array([ X[idx]-dx, Y[idx], Z[idx]]).T )
 gradx = (gradx_p - gradx_m)/ (2*dx)
-
+plt.plot(gradx)
+gradx = np.nan_to_num(gradx, nan =  0)
 grady_p = griddata( f_inter_input, Rad_den[idxnew], method = 'linear',
                     xi = np.array([ X[idx], Y[idx]+dx, Z[idx]]).T )
 grady_m = griddata( f_inter_input, Rad_den[idxnew], method = 'linear',
                     xi = np.array([ X[idx], Y[idx]-dx, Z[idx]]).T )
 grady = (grady_p - grady_m)/ (2*dx)
+plt.plot(grady)
+grady = np.nan_to_num(grady, nan =  0)
 
 gradz_p = griddata( f_inter_input, Rad_den[idxnew], method = 'linear',
                     xi = np.array([ X[idx], Y[idx], Z[idx]+dx]).T )
@@ -269,6 +288,7 @@ gradz_m = griddata( f_inter_input, Rad_den[idxnew], method = 'linear',
 # some nans here
 gradz_m = np.nan_to_num(gradz_m, nan =  0)
 gradz = (gradz_p - gradz_m)/ (2*dx)
+plt.plot(gradz)
 grad = np.sqrt(gradx**2 + grady**2 + gradz**2)
 gradr = (mu_x * gradx) + (mu_y*grady) + (mu_z*gradz)
 # v_grad = np.sqrt( (VX[idx] * gradx)**2 +  (VY[idx] * grady)**2 + (VZ[idx] * gradz)**2)
@@ -278,19 +298,23 @@ fld_factor = 3 * (1/np.tanh(R_lamda) - 1/R_lamda) / R_lamda
 
 from scipy.ndimage import uniform_filter1d # does moving mean without fucking the shape up
 smoothed_flux = -uniform_filter1d(r.T**2 * fld_factor * gradr / sigma_rossland_eval, 7) # i have removed the minus
-#%%
+#%% Spectra
+F_photo_temp = np.zeros((192, f_num))
 b = np.where( ((smoothed_flux>0) & (los<2/3) ))[0][0]
-
+# elad_b = 3117
+# b = elad_b
 Lphoto2 = 4*np.pi*c.c*smoothed_flux[b] * c.Msol_to_g / (c.t**2)
+EEr = Rad_den[idx]
 if Lphoto2 < 0:
     Lphoto2 = 1e100 # it means that it will always pick max_length for the negatives, maybe this is what we are getting wrong
-max_length = 4*np.pi*c.c*Rad_den[b]*r[b]**2 * c.Msol_to_g * c.Rsol_to_cm / (c.t**2)
+max_length = 4*np.pi*c.c*EEr[b]*r[b]**2 * c.Msol_to_g * c.Rsol_to_cm / (c.t**2)
 Lphoto = np.min( [Lphoto2, max_length])
-
+# Lphoto = Lphoto2
 # Spectra ---------------------------------------------------------------------
 los_effective[los_effective>30] = 30
 b2 = np.argmin(np.abs(los_effective-5))
-
+# elad_b2 = 3460
+# b2 = elad_b2
 for k in range(b2, len(r)):
     wien = np.exp(c.h * frequencies / (c.Kb * t[k])) - 1
     black_body = frequencies**3 / (c.c**2 * wien)
@@ -315,18 +339,20 @@ def temperature(n):
 elad_T = np.array([ temperature(n) for n in mat['nu']])
 for obs in range(1):
     y = np.multiply(mat['nu'], mat['F_photo_temp'][obs])
-    plt.loglog(elad_T, y, c='b', linestyle = '--', label ='Elad')
+    plt.loglog(elad_T, y, c='r', ls = ' ', label ='Elad', zorder = 4,
+               marker = 'o', markersize = 2)
     
 # us
-y_us = F_photo_temp[0] * frequencies
+y_us = F_photo_temp[i] * frequencies
 # temp_us = [temperature(n) for n in frequencies]
-plt.loglog(elad_T, np.abs(y_us), c = 'k',label='us')
+plt.loglog(elad_T, np.abs(y_us), c = 'k',label='us',
+           ls = ' ', marker = 'o', markersize = 5)
 
 # pretty
 x_start = 1e3
 x_end = 1e8
-y_lowlim = 1e10#2e39
-y_highlim = 1.3e54
+y_lowlim = 1e35#2e39
+y_highlim = 1.3e43
 plt.xlim(x_start,x_end)
 plt.ylim(y_lowlim, y_highlim)
 plt.loglog()
