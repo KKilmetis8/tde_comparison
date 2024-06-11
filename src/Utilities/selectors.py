1884a7f63b29a91086d885d2e8874e6fe149bba7
spaceturtle707 <p.martire98@gmail.com>
2024-06-11 09:05:46 +0200

    fixed selectors for alice

--- src/Utilities/selectors.py
+++ src/Utilities/selectors.py
@@ -39,10 +39,12 @@ def select_snap(m, mstar, rstar, check, time = True):
         if m == 4 and check == 'S60ComptonHires':
             snapshots = np.arange(210, 278 + 1)
         if m == 5 and check == 'fid':
-            snapshots = [245,246]#np.arange(100,365+1) 
+            snapshots = [245,246,247]#np.arange(100,365+1) 
         # select just the ones that actually exist
         snapshots = [snap for snap in snapshots if os.path.exists(f'{pre}{snap}/snap_{snap}.h5')]
-        snapshots = np.concatenate(snapshots)
+        print('before', snapshots)
+        snapshots = np.array(snapshots)
+        print('after', snapshots)
     else:
         if m == 4 and check == 'fid':
             snapshots = [293,322] #, 254, 263, 277 , 293, 308, 322]
