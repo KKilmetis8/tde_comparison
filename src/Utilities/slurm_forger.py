3cf37a17120b81f5834bf43300c0c488de8973d2
KKilmetis8 <asansol2552@gmail.com>
2025-02-18 16:27:46 +0100

    forger update

--- src/Utilities/slurm_forger.py
+++ src/Utilities/slurm_forger.py
@@ -13,39 +13,34 @@ suffix = 'beta1S60n1.5Compton'
 res = 'f'  # 'f' for fiducial, 'h' for HiRes, 's' for super high res,
            # 'd' double rad
 if Mbh == '10000':
-    runnos = np.arange(0, 22) + 1
-    
-    firsts = [80, 90, 100, 121, 142, 147, 153, 159, 163, 169, 175, 180,
-              184, 205, 226, 247, 268, 288, 304, 309, 323, 330, 343]
-    lasts =  [90, 100, 120, 141, 146, 152, 158, 162, 168, 174, 179, 183,
-              204, 225, 246, 267, 287, 303, 308, 322, 343, 348]
+    start = 90
+    end = 348
+    jobs = 40
+    edges = np.linspace(start, end, jobs + 1, dtype=int)
+    firsts = edges[:-1]
+    lasts = edges[1:] - 1  # Ensure last value is included in interval
+    lasts[-1] = end  # Make sure the last interval ends at 'end'
+    runnos = np.arange(0, len(firsts)) + 1
 if Mbh == '10_000' and res == 'h':
-    first = 80
-    last = 232
-    firsts = np.arange(80, 232, step=4)    
-    lasts = np.arange(83, 232, step=4)
-    lasts[-1] += 1 # turn the last one from 231 to 232
-    runnos = np.arange(0, 38) + 1
-
+    print('notreadyyet')
 if Mbh == '100000':
-    runnos = np.arange(0, 17) + 1
-    firsts = [132, 140, 151, 162, 173, 184, 195, 200, 221, 242, 263,
-              284, 305, 326, 344, 347, 363]
-    lasts = [140, 150, 161, 172, 183, 194, 199, 220, 241, 262, 283, 
-             304, 325, 343, 346, 362, 365]
+    start = 135
+    end = 365
+    jobs = 50
+    edges = np.linspace(start, end, jobs + 1, dtype=int)
+    firsts = edges[:-1]
+    lasts = edges[1:] - 1  
+    lasts[-1] = end  
+    runnos = np.arange(0, len(firsts)) + 1
 if Mbh == '1e+06':
-    firsts = [180, 186, 192, 196, 200, 211, 215, 219, 222, 225, 227, 229, 
-              233, 234, 239, 244, 246, 249, 252, 255, 257, 260, 263, 266, 
-              269, 272, 274, 277, 281, 284, 287, 294, 297, 299, 310, 321,
-              332, 334, 339, 343, 354, 366, 371, 377, 383, 389, 395, 401,
-              404, 408, 411, 414, 425, 430, 435, 440,]
-    lasts =  [185, 191, 195, 199, 210, 214, 218, 221, 224, 226, 228, 232,
-              233, 238, 243, 245, 248, 251, 254, 256, 259, 262, 265, 268, 
-              271, 273, 276, 280, 283, 286, 288, 296, 298, 309, 320, 331, 
-              333, 338, 342, 353, 365, 370, 376, 382, 388, 394, 400, 403, 
-              407, 410, 413, 420, 429, 434, 439, 444]
-    runnos = np.arange(0,len(firsts)) + 1
-
+    start = 200
+    end = 444
+    jobs = 60
+    edges = np.linspace(start, end, jobs + 1, dtype=int)
+    firsts = edges[:-1]
+    lasts = edges[1:] - 1  
+    lasts[-1] = end  
+    runnos = np.arange(0, len(firsts)) + 1
 
 for runno, first, last in zip(runnos, firsts, lasts):
     # Open file and read it
